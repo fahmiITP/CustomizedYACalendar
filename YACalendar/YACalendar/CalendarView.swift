f751b6a26d26a3978bbcef4c0447fe06c37c6130
Fahmi ITP <fahmi@itprovent.com>
2020-10-13 15:07:36 +0700

    Revert "04: Indates selection now uses end month date instead of start month date (#4)"
    This reverts commit ef1ca429340ab25eeb41340168072f1c4a788ea2.

--- YACalendar/YACalendar/CalendarView.swift
+++ YACalendar/YACalendar/CalendarView.swift
@@ -339,7 +339,7 @@ public class CalendarView: UIView {
                 if tappedMonth.containsDate(selectedDate) {
                     calendarDelegate?.didSelectDate?(selectedDate)
                 } else {
-                    calendarDelegate?.didSelectDate?(tappedMonth.endMonthDate)
+                    calendarDelegate?.didSelectDate?(tappedMonth.startMonthDate)
                 }
             } else {
                 calendarDelegate?.didSelectDate?(tappedMonth.startMonthDate)
